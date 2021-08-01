f59f0a13b55fb67ca0272022e3deacc46468f2be
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-08-02 00:50:11 +0300

    Modify css, html and javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const menuLinks = document.getElementsByClassName('menu')[0];
-
-toggleButton.addEventListener('click', () => {
-    menuLinks.classList.toggle('active') 
-})
-
-
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const menuLinks = document.getElementsByClassName("menu")[0];
 
